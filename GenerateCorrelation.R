89efd5f3c80dab9c8605c8f68e4efeed47cae448
murp9971 <38695469+murp9971@users.noreply.github.com>
2018-04-24 15:31:58 -0400

    Add files via upload
    R Script for generating correlation matrix

---
+++ GenerateCorrelation.R
@@ -0,0 +1,33 @@
+'**********************************************************************
+
+      SCRIPT TO GENERATE CORRELATION FOR OPIOID TABLEAU DASHBOARD
+
+**********************************************************************'
+
+setwd("C:/ECE/Tableau/Capstone/Capstone Opiate")
+
+if("RODBC" %in% rownames(installed.packages()) == FALSE) {install.packages("RODBC")}
+if("dplyr" %in% rownames(installed.packages()) == FALSE) {install.packages("dplyr")}
+if("corrplot" %in% rownames(installed.packages()) == FALSE) {install.packages("corrplot")}
+
+library(RODBC)
+library(dplyr)
+library(corrplot)
+
+# Create Connection - Use DNS setup on laptop/server
+dbConn <- odbcConnect("RConnection",uid="TableauUser", pwd="Tableau")
+
+# Run query retrieve data select two columns generate correlation
+df <- sqlQuery(dbConn, paste("select [State], count(distinct NPI) as 'Total NPIs', Deaths as 'Overdose Deaths'", 
+                             "from ECE.[dbo].[cms-data-load]",
+                             "group by [State], Deaths"))  %>%
+      select('Total NPIs', 'Overdose Deaths') %>%
+      cor() 
+
+
+# Save the object to a file to be loaded into Tableau 
+save(df, file="NPI_Overdose_Deaths.RData")
+
+
+# Close the connection
+odbcClose(dbConn)
